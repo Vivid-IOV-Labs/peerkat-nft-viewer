de5535cff98a06a564c68956bd89181bdd6dd248
bolladaniele <bolladaniele@gmail.com>
2021-10-15 16:12:33 +0100

    add vue axe

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import { createApp } from "vue";
+import { createApp, h, Fragment } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import router from "./router";
 import store from "./store";
 import { i18n } from "./i18n";
-import VueAxe from "vue-axe";
+import VueAxe, { VueAxePopup } from "vue-axe";
 
 if (process.env.NODE_ENV === "development") {
-  createApp(App)
+  createApp({
+    render: () => h(Fragment, [h(App), h(VueAxePopup)]),
+  })
+    .use(VueAxe)
     .use(router)
     .use(store)
     .use(i18n)
-    .use(VueAxe.default)
     .mount("#app");
 } else {
   createApp(App).use(router).use(store).use(i18n).mount("#app");
