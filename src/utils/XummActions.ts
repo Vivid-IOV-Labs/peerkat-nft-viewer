662e2cb0d7bf58ac8eadad790884d60af46ced7a
bolladaniele <bolladaniele@gmail.com>
2021-11-24 12:59:00 +0000

    update post mesasge

--- src/utils/XummActions.ts
+++ src/utils/XummActions.ts
@@ -1,18 +1,12 @@
 function command(message: any) {
-  if (
-    window.ReactNativeWebView &&
-    typeof window.ReactNativeWebView !== "undefined"
-  ) {
+  if (typeof window.ReactNativeWebView !== "undefined") {
     window.ReactNativeWebView.postMessage(JSON.stringify(message));
   }
 }
 
 export function openBrowser(url: string): void {
-  window.ReactNativeWebView.postMessage(
-    JSON.stringify({
-      command: "openBrowser",
-      url,
-    })
-  );
-  // command("openBrowser", { url });
+  command({
+    command: "openBrowser",
+    url,
+  });
 }
