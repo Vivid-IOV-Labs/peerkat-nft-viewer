6d07460826ead8204b01492d553eddd962255683
bolladaniele <bolladaniele@gmail.com>
2021-12-28 12:57:29 +0000

    log

--- src/store/modules/xumm/actions.ts
+++ src/store/modules/xumm/actions.ts
@@ -6,6 +6,7 @@ const actions: ActionTree<XummState, xAppOttData> = {
   async getOttData({ commit }): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const XummService = require("../../../services/XummService");
+    console.log("XummService", XummService);
     const OttData = await XummService.getOttData();
     commit("setOttData", OttData);
   },
