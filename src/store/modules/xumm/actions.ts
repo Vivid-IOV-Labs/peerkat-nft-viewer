49ce6acd2f399f1dc75c64102e62edae1bf3bcfb
bolladaniele <bolladaniele@gmail.com>
2021-12-28 13:13:16 +0000

    log

--- src/store/modules/xumm/actions.ts
+++ src/store/modules/xumm/actions.ts
@@ -1,12 +1,10 @@
 import { ActionTree } from "vuex";
 import { xAppOttData } from "xumm-sdk/dist/src/types";
 import { XummState } from "./state";
+import XummService from "../../../services/XummService";
 
 const actions: ActionTree<XummState, xAppOttData> = {
   async getOttData({ commit }): Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const XummService = require("../../../services/XummService").default;
-    console.log("XummService", XummService);
     const OttData = await XummService.getOttData();
     commit("setOttData", OttData);
   },
