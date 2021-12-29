4f9d76bd4bb1caa9a4441d6d10cbf2c1113aa6ef
bolladaniele <bolladaniele@gmail.com>
2021-12-29 12:06:16 +0000

    insert secret

--- src/services/XummService.ts
+++ src/services/XummService.ts
@@ -1,57 +1,8 @@
 const xummApiKey = import.meta.env.VITE_XUMM_API_KEY as string;
+const xummApiSecret = import.meta.env.VITE_XUMM_API_SECRET as string;
 import type { XummTypes } from "xumm-sdk";
 
 import { xAppOttData } from "xumm-sdk/dist/src/types";
-// let Sdk: any = null;
-// function initialize() {
-//   // eslint-disable-next-line @typescript-eslint/no-var-requires
-//   const { XummSdkJwt } = require("xumm-sdk");
-//   Sdk = new XummSdkJwt(xummApiKey);
-//   const redirect = Sdk.xApp.get("redirect");
-// }
-// export async function getOttData(): Promise<xAppOttData> {
-//   if (Sdk) {
-//     const ottdata = await Sdk.getOttData();
-//     return ottdata;
-//   } else {
-//     initialize();
-//     const ottdata = await Sdk.getOttData();
-//     return ottdata;
-//   }
-// }
-
-// export async function createPaylod(
-//   newPayload: XummTypes.CreatePayload
-// ): Promise<any> {
-//   if (Sdk) {
-//     const created = await Sdk.payload.create(newPayload);
-
-//     return created;
-//   } else {
-//     initialize();
-//     const created = await Sdk.payload.create(newPayload);
-
-//     return created;
-//   }
-// }
-
-// export async function saveToStorage(toStore: any) {
-//   if (Sdk) {
-//     await Sdk.storage.set(toStore);
-//   } else {
-//     initialize();
-//     await Sdk.storage.set(toStore);
-//   }
-// }
-
-// export async function getStorage() {
-//   if (Sdk) {
-//     return await Sdk.storage.get();
-//   } else {
-//     initialize();
-//     return await Sdk.storage.get();
-//   }
-// }
 
 let Sdk: any = null;
 
@@ -62,7 +13,7 @@ class XummService {
     }
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { XummSdkJwt } = require("xumm-sdk");
-    Sdk = new XummSdkJwt(xummApiKey);
+    Sdk = new XummSdkJwt(xummApiKey, xummApiSecret);
   }
   async getOttData(): Promise<xAppOttData> {
     const ottdata = await Sdk.getOttData();
