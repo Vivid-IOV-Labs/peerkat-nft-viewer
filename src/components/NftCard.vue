7ebd1dc99f96cd92e0bbe8df45773716ce5849c9
bolladaniele <bolladaniele@gmail.com>
2022-01-11 13:51:04 +0000

    view

--- src/components/NftCard.vue
+++ src/components/NftCard.vue
@@ -2,22 +2,24 @@
   <base-card style="height: 100%">
     <template #picture>
       <figure class="h-50">
-        <video
-          v-if="nft.media_type?.includes('video')"
-          :src="`${nft.url}#t=0.5`"
-          preload="metadata"
-          muted
-          class="img-fluid card-img-top"
-          style="object-fit: cover; height: 100%; object-position: center top"
-        ></video>
-        <img
-          v-else
-          style="object-fit: cover; height: 100%; object-position: center top"
-          class="img-fluid card-img-top"
-          :src="nft.url"
-          alt="Card image cap"
-          @error="fallbackImg"
-        />
+        <a href="#" @click.prevent="view">
+          <video
+            v-if="nft.media_type?.includes('video')"
+            :src="`${nft.url}#t=0.5`"
+            preload="metadata"
+            muted
+            class="img-fluid card-img-top"
+            style="object-fit: cover; height: 100%; object-position: center top"
+          ></video>
+          <img
+            v-else
+            style="object-fit: cover; height: 100%; object-position: center top"
+            class="img-fluid card-img-top"
+            :src="nft.url"
+            alt="Card image cap"
+            @error="fallbackImg"
+          />
+        </a>
       </figure>
     </template>
 
@@ -49,7 +51,6 @@
         >
           Inspect</external-link
         >
-        <base-button class="mr-2" @click="view">View</base-button>
       </div>
     </template>
   </base-card>
