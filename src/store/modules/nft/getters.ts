1a00ff0d68513361446adfb31f43fd37053dc3ed
bolladaniele <bolladaniele@gmail.com>
2022-01-13 12:59:45 +0000

    test network share

--- src/store/modules/nft/getters.ts
+++ src/store/modules/nft/getters.ts
@@ -6,19 +6,19 @@ interface NFT {
 }
 export default {
   getAll: (state: NFTState): Array<NFT> => state.all,
-  getShared:
-    (state: NFTState) =>
-    (nodetype: keyof SharedNFTs): Array<NFT> =>
-      state.shared[nodetype],
-  getLines: (state: NFTState): Array<NFT> => state.lines,
+  // getShared:
+  //   (state: NFTState) =>
+  //   (nodetype: keyof SharedNFTs): Array<NFT> =>
+  //     state.shared[nodetype],
+  // getLines: (state: NFTState): Array<NFT> => state.lines,
   getByAddress:
     (state: NFTState) =>
     (address: string): NFT | undefined => {
       return state.all.find(({ issuer }) => issuer == address);
     },
-  getSharedByAddress:
-    (state: NFTState) =>
-    (address: string, nodetype: keyof SharedNFTs): NFT | undefined => {
-      return state.shared[nodetype].find(({ issuer }) => issuer == address);
-    },
+  // getSharedByAddress:
+  //   (state: NFTState) =>
+  //   (address: string, nodetype: keyof SharedNFTs): NFT | undefined => {
+  //     return state.shared[nodetype].find(({ issuer }) => issuer == address);
+  //   },
 };
