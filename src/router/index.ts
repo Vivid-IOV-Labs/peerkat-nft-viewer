7a0e0b0b5ef0f291fa1b73d5d92066abec294799
bolladaniele <bolladaniele@gmail.com>
2022-01-13 13:22:08 +0000

    test network share

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,56 +19,56 @@ const routes = [
         name: "MyNFTs",
         component: () => import("../views/MyNFTs.vue"),
       },
-      // {
-      //   path: ":nftAddress/view",
-      //   name: "NFTWalletView",
-      //   component: () => import("../views/NFTWalletView.vue"),
-      //   meta: {
-      //     announcer: {
-      //       message: "NFT View Page",
-      //     },
-      //   },
-      // },
+      {
+        path: ":nftAddress/view",
+        name: "NFTWalletView",
+        component: () => import("../views/NFTWalletView.vue"),
+        meta: {
+          announcer: {
+            message: "NFT View Page",
+          },
+        },
+      },
     ],
   },
-  // {
-  //   path: "/shared",
-  //   name: "Shared",
-  //   component: () => import("../views/Shared.vue"),
-  //   meta: {
-  //     withAuth: true,
-  //     title: "Home Wallet Page",
-  //   },
-  //   children: [
-  //     {
-  //       path: "",
-  //       name: "SharedNFTs",
-  //       component: () => import("../views/SharedNFTs.vue"),
-  //     },
-  //     {
-  //       path: ":nftAddress/:nodetype/:currency?",
-  //       name: "NFTSharedDetail",
-  //       component: () => import("../views/NFTSharedDetail.vue"),
-  //       meta: {
-  //         withAuth: true,
-  //         announcer: {
-  //           message: "NFT Shared Detail Page",
-  //         },
-  //       },
-  //     },
-  //     {
-  //       path: ":nftAddress/:nodetype/view",
-  //       name: "NFTSharedView",
-  //       component: () => import("../views/NFTSharedView.vue"),
-  //       meta: {
-  //         withAuth: true,
-  //         announcer: {
-  //           message: "NFT Shared View Page",
-  //         },
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    path: "/shared",
+    name: "Shared",
+    component: () => import("../views/Shared.vue"),
+    meta: {
+      withAuth: true,
+      title: "Home Wallet Page",
+    },
+    children: [
+      {
+        path: "",
+        name: "SharedNFTs",
+        component: () => import("../views/SharedNFTs.vue"),
+      },
+      {
+        path: ":nftAddress/:nodetype/:currency?",
+        name: "NFTSharedDetail",
+        component: () => import("../views/NFTSharedDetail.vue"),
+        meta: {
+          withAuth: true,
+          announcer: {
+            message: "NFT Shared Detail Page",
+          },
+        },
+      },
+      {
+        path: ":nftAddress/:nodetype/view",
+        name: "NFTSharedView",
+        component: () => import("../views/NFTSharedView.vue"),
+        meta: {
+          withAuth: true,
+          announcer: {
+            message: "NFT Shared View Page",
+          },
+        },
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
