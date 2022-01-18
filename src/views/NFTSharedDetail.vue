48b47a7206057dc0e3f26a0405adcc2d056087d2
bolladaniele <bolladaniele@gmail.com>
2022-01-18 16:23:47 +0000

    WIP

--- src/views/NFTSharedDetail.vue
+++ src/views/NFTSharedDetail.vue
@@ -80,10 +80,7 @@ export default defineComponent({
     const client = await init(nodetype);
     const nft = ref<NFT | null>(null);
     try {
-      nft.value = await client.fetchOne(
-        route.params.nftAddress.toString(),
-        route.params.currency.toString()
-      );
+      nft.value = await client.fetchOne(route.params.nftAddress.toString());
       console.log("nft/addShared", nodetype);
       store.commit("nft/addShared", {
         shared: nft.value,
