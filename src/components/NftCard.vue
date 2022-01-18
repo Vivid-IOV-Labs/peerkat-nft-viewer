6a6ecae3df91f99b0f8e1e60c8e43e156b260f29
bolladaniele <bolladaniele@gmail.com>
2022-01-18 11:07:46 +0000

    fix view shared link

--- src/components/NftCard.vue
+++ src/components/NftCard.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="view">
           <video
             v-if="nft.media_type?.includes('video')"
-            :src="`${nft.url}#t=0.5`"
+            :src="`${nft.url}`"
             muted
             class="img-fluid card-img-top"
             style="object-fit: cover; height: 100%; object-position: center top"
