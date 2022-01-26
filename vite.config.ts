2d10cc871107bdcd53db3563dd2f76669110a7eb
bolladaniele <bolladaniele@gmail.com>
2022-01-26 11:48:48 +0000

    inject script

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
+import { injectHtml } from "vite-plugin-html";
+
+const xummSandbox = process.env.VITE_XUMM_SANDOX;
+console.log(xummSandbox);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({})],
+  plugins: [
+    vue(),
+    VitePWA({}),
+    injectHtml({
+      data: {
+        title: "Peerkat",
+        injectScript:
+          '<script data-consolejs-channel="0dc99d93-4c6b-6c36-8f48-e404d46bb556" src="https://remotejs.com/agent/agent.js"></script>',
+      },
+    }),
+  ],
   optimizeDeps: {
     include: ["axe-core"],
   },
