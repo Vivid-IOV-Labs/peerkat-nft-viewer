ac3f39be15b5bb55a11d7bb9f5b0edd2bb52bb86
bolladaniele <bolladaniele@gmail.com>
2022-01-27 13:50:15 +0000

    add tag manager

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,8 +1,16 @@
 /* eslint-disable no-undef */
 const mixpanel_id = import.meta.env.VITE_MIXPANEL_ID;
 const google_analytics_id = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+const google_tag_manager_id = import.meta.env.VITE_GOOGLE_TAG_MANAGER;
+
 import mixpanel from "mixpanel-browser";
-import { devlog } from "./devlog";
+
+const dataLayer = (window as any).dataLayer || [];
+function gtag(...args: (string | boolean | Date | undefined)[]) {
+  dataLayer.push(args);
+}
+console.log(dataLayer);
+
 const ga = (window as any).ga;
 interface trackEventParams {
   category: string;
@@ -17,16 +25,28 @@ export function trackInit(): void {
   });
   ga("create", google_analytics_id, "auto");
   ga("send", "pageview");
+  gtag("js", new Date());
+
+  gtag("config", google_tag_manager_id);
 }
 
 export const trackUser = (walletAddress: string): void => {
   mixpanel.identify(walletAddress);
   ga("send", "&uid", walletAddress);
+  dataLayer.push({
+    "&uid": walletAddress,
+  });
 };
 
 export const trackPage = (path: string): void => {
   mixpanel.track(`Page view: ${path}`);
   ga("send", "pageview", path);
+  dataLayer.push({
+    event: "pageview",
+    page: {
+      url: path,
+    },
+  });
 };
 
 export const trackEvent = ({
@@ -34,10 +54,14 @@ export const trackEvent = ({
   action,
   label,
 }: trackEventParams): void => {
-  try {
-    mixpanel.track(`${category}`, { action, label });
-    ga("send", "event", category, action, label);
-  } catch (error) {
-    devlog(error);
-  }
+  mixpanel.track(`${category}`, { action, label });
+  ga("send", "event", category, action, label);
+  dataLayer.push({
+    event: "event",
+    eventProps: {
+      category: category,
+      action: action,
+      label: label,
+    },
+  });
 };
