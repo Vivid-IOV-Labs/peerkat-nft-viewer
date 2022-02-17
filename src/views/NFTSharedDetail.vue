72866f2d6f726a5466e3d1efd1bd276863ab4070
bolladaniele <bolladaniele@gmail.com>
2022-02-17 17:18:39 +0000

    WIP

--- src/views/NFTSharedDetail.vue
+++ src/views/NFTSharedDetail.vue
@@ -3,7 +3,7 @@
     >Back
   </router-link>
 
-  <div style="overflo-y: auto">
+  <div style="overflow-y: auto">
     <base-card v-if="nft">
       <template #picture>
         <figure style="overflow: hidden; height: 100%">
