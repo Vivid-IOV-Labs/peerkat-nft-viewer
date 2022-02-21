9bd9cc1c9342c57da1f7466b87ea2da7bcac7f39
bolladaniele <bolladaniele@gmail.com>
2022-02-21 11:28:23 +0000

    Logging error on populate

--- src/utils/devlog.ts
+++ src/utils/devlog.ts
@@ -1,4 +1,4 @@
-export function devlog(...args: any): void {
+export function devlog(...args: unknown[]): void {
   if (process.env.NODE_ENV === "development") {
     // eslint-disable-next-line no-console
     console.log(...args);
