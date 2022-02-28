29bb76deff716b8164538e01be5b53b36fe0ea40
bolladaniele <bolladaniele@gmail.com>
2022-02-28 16:17:22 +0000

    load every 2

--- src/store/modules/nft/actions.ts
+++ src/store/modules/nft/actions.ts
@@ -37,7 +37,7 @@ const actions: ActionTree<NFT, NFTState> = {
   async fetchNext({ commit, getters }): Promise<void> {
     const client = getters.getXrpClient;
     const count = getters.getAll.length;
-    const nextLines = getters.getLines.slice(count, count + 3);
+    const nextLines = getters.getLines.slice(count, count + 2);
     const nextNfts: NFT[] = await client.fetchNext(nextLines);
     commit("setAll", nextNfts);
   },
