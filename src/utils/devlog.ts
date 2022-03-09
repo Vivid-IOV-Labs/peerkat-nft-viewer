1120d087a4e41d2cee5582d0d6288339551ff884
bolladaniele <bolladaniele@gmail.com>
2022-03-09 15:24:32 +0000

    devlog also in staging

--- src/utils/devlog.ts
+++ src/utils/devlog.ts
@@ -1,5 +1,5 @@
 export function devlog(...args: unknown[]): void {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV !== "production") {
     // eslint-disable-next-line no-console
     console.log(...args);
   } else {
