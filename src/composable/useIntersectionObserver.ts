cc0a7087604f3af5a3b511ba189ebfcd8ab0fb35
bolladaniele <bolladaniele@gmail.com>
2022-03-14 10:25:57 +0000

    reduce intersection margin

--- src/composable/useIntersectionObserver.ts
+++ src/composable/useIntersectionObserver.ts
@@ -33,7 +33,7 @@ export default function useIntersectionObserver(
 
         isIntersecting.value = false;
       },
-      { root: root.value, rootMargin: "0px 500px" }
+      { root: root.value, rootMargin: "0px 200px" }
     );
     observe();
   });
