fbb8477a3f5f1f14f4a7579135570043faec7a37
bolladaniele <bolladaniele@gmail.com>
2022-03-14 09:17:26 +0000

    removes extra env variables

--- src/utils/websocketAdaptor.ts
+++
@@ -1,6 +0,0 @@
-import socketIOClient from "socket.io-client";
-import sailsIOClient from "sails.io.js";
-const apiUrl = import.meta.env.VITE_API_URL;
-const io = sailsIOClient(socketIOClient);
-io.sails.url = apiUrl;
-export default io;
