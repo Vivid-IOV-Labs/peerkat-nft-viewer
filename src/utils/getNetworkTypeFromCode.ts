63e97219062a70b7c2ca052d9a8b4c2c0ef95a78
bolladaniele <bolladaniele@gmail.com>
2022-03-23 12:01:51 +0000

    add dev and custom networks support

--- src/utils/getNetworkTypeFromCode.ts
+++ src/utils/getNetworkTypeFromCode.ts
@@ -5,9 +5,21 @@ export function getNetworkTypeFromCode(code: number): string | undefined {
   if (code == 1) {
     return "TESTNET";
   }
+  if (code == 2) {
+    return "DEVNET";
+  }
+  if (code == 3) {
+    return "CUSTOM";
+  }
 }
 
 export function getNetworkCodeFromType(type: string): number | undefined {
+  if (type == "CUSTOM") {
+    return 3;
+  }
+  if (type == "DEVNET") {
+    return 2;
+  }
   if (type == "TESTNET") {
     return 1;
   }
