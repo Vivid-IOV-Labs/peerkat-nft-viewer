fb6e07e372f8d49aa83cedfb263da9f109d69a8e
bolladaniele <bolladaniele@gmail.com>
2022-04-20 11:23:26 +0100

    getinspectorurl

---
+++ src/utils/getInspectorUrl.ts
@@ -0,0 +1,34 @@
+// const main_networks = [
+//   "wss://s2.ripple.com",
+//   "wss://xrpl.link",
+//   "wss://xrpcluster.com",
+// ];
+// const test_networks = [
+//   "wss://s.altnet.rippletest.net:51233",
+//   "wss://xrpl.linkwss://testnet.xrpl-labs.com",
+// ];
+// const custom_networks = ["wss://xls20-sandbox.rippletest.net:51233"];
+// const dev_networks = ["wss://s.devnet.rippletest.net:51233"];
+// const bithomUrls = [
+//   "https://test.bithomp.com/",
+//   "https://xls20.bithomp.com/",
+//   "https://hooks.bithomp.com/",
+//   "https://beta.bithomp.com/",
+// ];
+interface urlMap {
+  [id: string]: string[];
+}
+export function getInspectorUrl(network: keyof urlMap): string {
+  const urlMap: urlMap = {
+    /*test */
+    "wss://s.altnet.rippletest.net:51233": ["https://test.bithomp.com/"],
+    "wss://testnet.xrpl-labs.com": ["https://test.bithomp.com/"],
+    /*main */
+    "wss://s2.ripple.com": ["https://bithomp.com/"],
+    "wss://xrpl.link": ["https://bithomp.com/"],
+    "wss://xrpcluster.com": ["https://bithomp.com/"],
+    /*custom */
+    "xls20-sandbox.rippletest.net:51233": ["https://xls20.bithomp.com/"],
+  };
+  return urlMap[network][0];
+}
