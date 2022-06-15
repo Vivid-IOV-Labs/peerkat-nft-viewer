62096094a0d855df878ace7b749fb94223c7a96e
bolladaniele <bolladaniele@gmail.com>
2022-06-15 10:28:43 +0100

    wip

--- src/utils/isInXumm.ts
+++ src/utils/isInXumm.ts
@@ -2,9 +2,9 @@ export function isInXumm(): boolean {
   const urlParams = new URLSearchParams(document.location.href);
   const jwtToke = urlParams.get("xAppToken");
   const xAppTokenInUrl = urlParams.has("xAppToken");
+  console.log("document.location.href", document.location.href);
   console.log("urlParams", urlParams);
+  console.log("xAppToken", jwtToke);
   console.log("xAppTokenInUrl", xAppTokenInUrl);
-  console.log("jwtToke", jwtToke);
-
   return /xumm/.test(navigator.userAgent) || xAppTokenInUrl;
 }
