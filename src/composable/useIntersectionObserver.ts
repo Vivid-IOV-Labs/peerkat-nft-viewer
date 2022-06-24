4ab30ec21dd8d15479ce0cfab1ddae97dad83408
bolladaniele <bolladaniele@gmail.com>
2022-06-24 15:26:25 +0100

    wip

--- src/composable/useIntersectionObserver.ts
+++ src/composable/useIntersectionObserver.ts
@@ -27,7 +27,7 @@ export default function useIntersectionObserver(
 
         isIntersecting.value = false;
       },
-      { root: root.value, rootMargin: "0px 100px", threshold: 0 }
+      { root: root.value, rootMargin: "0px 60px", threshold: 0 }
     );
     observe();
   });
