7ec989ae934a13b6fa21d35883b5ea0dc8abaff3
bolladaniele <bolladaniele@gmail.com>
2022-08-24 16:00:01 +0100

    wip

--- src/utils/XummActions.ts
+++ src/utils/XummActions.ts
@@ -1,66 +1,63 @@
-import { devlog } from "./devlog";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const xAppSdk = require("xumm-sdk");
-const xapp = new xAppSdk();
-
-function command(message: any) {
-  if (typeof window.ReactNativeWebView !== "undefined") {
-    window.ReactNativeWebView.postMessage(JSON.stringify(message));
-  }
-  // else {
-  //   window && window.open(message, "_blank").focus();
-  // }
-}
-
-export function openBrowser(url: string): void {
-  // command({
-  //   command: "openBrowser",
-  //   url,
-  // });
-
-  xapp
-    .openBrowser({ url })
-    .then((d) => {
-      // d (returned value) can be Error or return data:
-      console.log("openBrowser response:", d instanceof Error ? d.message : d);
-    })
-    .catch((e) => console.log("Error:", e.message));
-}
-
-export function openSignRequest(uuid: string): void {
-  xapp
-    .openSignRequest({ uuid })
-    .then((d) => {
-      // d (returned value) can be Error or return data:
-      console.log(
-        "openSignRequest response:",
-        d instanceof Error ? d.message : d
-      );
-    })
-    .catch((e) => console.log("Error:", e.message));
-  // command({
-  //   command: "openSignRequest",
-  //   uuid,
-  // });
-  // devlog("Opensinge request uuid", uuid);
-}
+// import { devlog } from "./devlog";
+// const xapp = new xAppSdk();
 
 // function command(message: any) {
 //   if (typeof window.ReactNativeWebView !== "undefined") {
 //     window.ReactNativeWebView.postMessage(JSON.stringify(message));
 //   }
+//   // else {
+//   //   window && window.open(message, "_blank").focus();
+//   // }
 // }
 
 // export function openBrowser(url: string): void {
-//   command({
-//     command: "openBrowser",
-//     url,
-//   });
+//   // command({
+//   //   command: "openBrowser",
+//   //   url,
+//   // });
+
+//   xapp
+//     .openBrowser({ url })
+//     .then((d) => {
+//       // d (returned value) can be Error or return data:
+//       console.log("openBrowser response:", d instanceof Error ? d.message : d);
+//     })
+//     .catch((e) => console.log("Error:", e.message));
 // }
 
 // export function openSignRequest(uuid: string): void {
-//   command({
-//     command: "openSignRequest",
-//     uuid,
-//   });
+//   xapp
+//     .openSignRequest({ uuid })
+//     .then((d) => {
+//       // d (returned value) can be Error or return data:
+//       console.log(
+//         "openSignRequest response:",
+//         d instanceof Error ? d.message : d
+//       );
+//     })
+//     .catch((e) => console.log("Error:", e.message));
+//   // command({
+//   //   command: "openSignRequest",
+//   //   uuid,
+//   // });
+//   // devlog("Opensinge request uuid", uuid);
 // }
+function command(message: any) {
+  if (typeof window.ReactNativeWebView !== "undefined") {
+    window.ReactNativeWebView.postMessage(JSON.stringify(message));
+  }
+}
+
+export function openBrowser(url: string): void {
+  command({
+    command: "openBrowser",
+    url,
+  });
+}
+
+export function openSignRequest(uuid: string): void {
+  command({
+    command: "openSignRequest",
+    uuid,
+  });
+}
