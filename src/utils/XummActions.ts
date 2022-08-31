91ad175426cd509073907921e5e4fd88f5a7f31e
bolladaniele <bolladaniele@gmail.com>
2022-08-31 11:15:33 +0100

    wip

--- src/utils/XummActions.ts
+++ src/utils/XummActions.ts
@@ -1,17 +1,12 @@
 function command(message: any) {
   if (typeof window.ReactNativeWebView !== "undefined") {
-    console.log(" ReactNativeWebView", message);
-
     window.ReactNativeWebView.postMessage(JSON.stringify(message));
   } else {
     window.parent.postMessage(JSON.stringify(message), "*");
-    console.log("NO ReactNativeWebView");
   }
 }
 
 export function openBrowser(url: string): void {
-  console.log("openBrowser", url);
-
   command({
     command: "openBrowser",
     url,
