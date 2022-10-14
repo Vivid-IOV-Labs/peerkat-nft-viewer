3713152d9aab3da7a25ef4aed88b6d35e468c935
bolladaniele <bolladaniele@gmail.com>
2022-10-14 12:02:04 +0100

    add coranywhere

--- src/services/XrpService.ts
+++ src/services/XrpService.ts
@@ -784,8 +784,14 @@ interface list {
   [name: string]: any | undefined;
 }
 const ipfsGatewayLisWithObfuscateTime: any[] = [
-  { domain: "https://cf-ipfs.com/", obfuscateTime: null },
-  { domain: "https://cloudflare-ipfs.com/", obfuscateTime: null },
+  {
+    domain: "https://cors-anywhere.herokuapp.com/https://cf-ipfs.com/",
+    obfuscateTime: null,
+  },
+  {
+    domain: "https://cors-anywhere.herokuapp.com/https://cloudflare-ipfs.com/",
+    obfuscateTime: null,
+  },
   { domain: "https://gateway.ipfs.io/", obfuscateTime: null },
   { domain: "https://nftstorage.link/", obfuscateTime: null },
   { domain: "https://ipfs.io/", obfuscateTime: null },
