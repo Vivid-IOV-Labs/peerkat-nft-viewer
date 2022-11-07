e3aa8a4122a7beae1e46616e25137d1bd9e07a27
bolladaniele <bolladaniele@gmail.com>
2022-11-07 10:48:07 +0000

    wip

--- src/views/HoldingPage.vue
+++ src/views/HoldingPage.vue
@@ -4,5 +4,8 @@
     style="width: 60%; margin: 1rem auto; padding: 1rem"
   >
     <h1>Holding Page</h1>
+    <div class="card-text">
+      text holding page aasdasdas as dasd asd asd asd asd asd
+    </div>
   </div>
 </template>
