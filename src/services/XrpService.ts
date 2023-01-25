09fe93777544fcbf9db5829f590a432053c500e0
bolladaniele <bolladaniele@gmail.com>
2023-01-25 11:32:38 +0000

    using our cloud front bucket for metadata

--- src/services/XrpService.ts
+++ src/services/XrpService.ts
@@ -508,7 +508,7 @@ export async function fetchOneXls20(
     return n.NFTokenID == NFTokenID;
   });
   if (nftXLS20) {
-    return getOneXls(nftXLS20);
+    return getOneXls20(nftXLS20);
   } else {
     throw new Error("Not an XLS-20");
   }
@@ -533,7 +533,7 @@ function createUrlFromDomain(domain: string, nftokenid: string) {
   }
 }
 
-export async function getOneXls(nft: any) {
+export async function getOneXls20(nft: any) {
   let mediaUrl;
   let media_type;
   let error_code;
@@ -546,80 +546,92 @@ export async function getOneXls(nft: any) {
   let thumbnail;
   let details;
   const { Issuer, NFTokenID, URI, NFTokenTaxon, nft_serial } = nft;
-  if (!URI) {
-    const domain = await getDomain(Issuer);
-    const url = createUrlFromDomain(domain, NFTokenID);
-    try {
-      details = await fetch(url).then((r) => r.json());
-      if (details.code && details.code == 404) {
-        throw new Error();
+  try {
+    const url = `https://d2gdfyavin91j3.cloudfront.net/assets/metadata/${NFTokenID}/matadata.json`;
+    debugger;
+    details = await fetch(url).then((r) => r.json());
+  } catch (err) {
+    devlog(err);
+    debugger;
+    if (!URI) {
+      const domain = await getDomain(Issuer);
+      const url = createUrlFromDomain(domain, NFTokenID);
+      try {
+        details = await fetch(url).then((r) => r.json());
+        if (details.code && details.code == 404) {
+          throw new Error();
+        }
+      } catch (error) {
+        error_code = "no_nfts_in_collection";
+        error_title = "Data currently unavailable  [X02]";
+        error_message =
+          "This error may occur when the viewer is currently unable to fetch metadata from the URI. This error occurs when the viewer is not familiar with the URI approach. Please contact the Token Issuer for support. We will continue to upgrade the viewer, follow Peerkat via Twitter and Discord for updates and support.";
       }
-    } catch (error) {
-      error_code = "no_nfts_in_collection";
-      error_title = "Data currently unavailable  [X02]";
-      error_message =
-        "This error may occur when the viewer is currently unable to fetch metadata from the URI. This error occurs when the viewer is not familiar with the URI approach. Please contact the Token Issuer for support. We will continue to upgrade the viewer, follow Peerkat via Twitter and Discord for updates and support.";
-    }
-  } else {
-    const uri = hexToString(URI);
-    const end = uri.includes(".json")
-      ? ""
-      : Number.isInteger(NFTokenTaxon)
-      ? `/${NFTokenTaxon}.json`
-      : "/base.json";
+    } else {
+      const uri = hexToString(URI);
+      const end = uri.includes(".json")
+        ? ""
+        : Number.isInteger(NFTokenTaxon)
+        ? `/${NFTokenTaxon}.json`
+        : "/base.json";
 
-    const url = uri.includes("ipfs:")
-      ? uri.split("//")[1]
-      : uri.includes("/ipfs/")
-      ? uri.split("/ipfs/")[1]
-      : uri.includes("cid:")
-      ? uri.split("cid:")[1]
-      : uri;
-    /*
-    different kind of uri
-    cid:bafybeignu67z7yimitdl74tis4v6b47bbcuzzsp7d64v4psny4uqdcsvy4
-    https://bafybeignu67z7yimitdl74tis4v6b47bbcuzzsp7d64v4psny4uqdcsvy4.ipfs.w3s.link/metadata.json #https://([a-zA-Z]+([0-9]+[a-zA-Z]+)+)\.ipfs\.[A-Za-z0-9]+\.[A-Za-z0-9]+/([A-Za-z0-9]+(_[A-Za-z0-9]+)+)\.[A-Za-z0-9]+
-    bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi
-    ipfs://bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi/metadata.json
-    https://ipfs.io/ipfs/bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi/metadata.json
-    https://somedomain/bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi
-    */
+      const url = uri.includes("ipfs:")
+        ? uri.split("//")[1]
+        : uri.includes("/ipfs/")
+        ? uri.split("/ipfs/")[1]
+        : uri.includes("cid:")
+        ? uri.split("cid:")[1]
+        : uri;
+      /*
+      different kind of uri
+      cid:bafybeignu67z7yimitdl74tis4v6b47bbcuzzsp7d64v4psny4uqdcsvy4
+      https://bafybeignu67z7yimitdl74tis4v6b47bbcuzzsp7d64v4psny4uqdcsvy4.ipfs.w3s.link/metadata.json #https://([a-zA-Z]+([0-9]+[a-zA-Z]+)+)\.ipfs\.[A-Za-z0-9]+\.[A-Za-z0-9]+/([A-Za-z0-9]+(_[A-Za-z0-9]+)+)\.[A-Za-z0-9]+
+      bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi
+      ipfs://bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi/metadata.json
+      https://ipfs.io/ipfs/bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi/metadata.json
+      https://somedomain/bafybeibxjchfxkfcki4dtmums24fgxyjot52sklnzpphm4fl2vd5dypdxi
+      */
 
-    try {
-      const response =
-        uri.includes("ipfs:") ||
-        uri.includes("/ipfs/") ||
-        !uri.includes("//") ||
-        uri.includes("cid:")
-          ? await getIpfsJson(url)
-          : await fetch(url);
-      if (response.headers) {
-        const contentType = response.headers.get("Content-Type");
-        if (contentType?.includes("image") || contentType?.includes("video")) {
-          const ipfLinkUrlPattern = new RegExp(
-            "https://([a-zA-Z]+([0-9]+[a-zA-Z]+)+).ipfs.[A-Za-z0-9]+.[A-Za-z0-9]+/([A-Za-z0-9]+(_[A-Za-z0-9]+)+).[A-Za-z0-9]+"
-          ).test(url);
-          if (ipfLinkUrlPattern) {
-            const ipfsHash = url.split(".ipfs")[0].split("//")[1];
-            const name = url.split(".ipfs")[1].split("/")[1];
-            thumbnail = ipfsHash + "/" + name;
-            media_type = contentType;
-            mediaUrl = ipfsHash + "/" + name;
+      try {
+        const response =
+          uri.includes("ipfs:") ||
+          uri.includes("/ipfs/") ||
+          !uri.includes("//") ||
+          uri.includes("cid:")
+            ? await getIpfsJson(url)
+            : await fetch(url);
+        if (response.headers) {
+          const contentType = response.headers.get("Content-Type");
+          if (
+            contentType?.includes("image") ||
+            contentType?.includes("video")
+          ) {
+            const ipfLinkUrlPattern = new RegExp(
+              "https://([a-zA-Z]+([0-9]+[a-zA-Z]+)+).ipfs.[A-Za-z0-9]+.[A-Za-z0-9]+/([A-Za-z0-9]+(_[A-Za-z0-9]+)+).[A-Za-z0-9]+"
+            ).test(url);
+            if (ipfLinkUrlPattern) {
+              const ipfsHash = url.split(".ipfs")[0].split("//")[1];
+              const name = url.split(".ipfs")[1].split("/")[1];
+              thumbnail = ipfsHash + "/" + name;
+              media_type = contentType;
+              mediaUrl = ipfsHash + "/" + name;
+            }
+          } else {
+            details = await response.json();
           }
         } else {
-          details = await response.json();
+          details = response;
         }
-      } else {
-        details = response;
+      } catch (error) {
+        devlog(error);
+        error_code = "no_nfts_in_collection";
+        error_title = "Data currently unavailable  [X01]";
+        error_message =
+          "This error may occur when the viewer attempts to fetch metadata from the URI and the network request times out. This error occurs most frequently when using a public IPFS link. Please try again by quitting the xApp and reload. We will continue to upgrade the viewer, follow Peerkat via Twitter and Discord for updates and support.";
       }
-    } catch (error) {
-      devlog(error);
-      error_code = "no_nfts_in_collection";
-      error_title = "Data currently unavailable  [X01]";
-      error_message =
-        "This error may occur when the viewer attempts to fetch metadata from the URI and the network request times out. This error occurs most frequently when using a public IPFS link. Please try again by quitting the xApp and reload. We will continue to upgrade the viewer, follow Peerkat via Twitter and Discord for updates and support.";
     }
   }
+
   if (details) {
     tokenName = details.name && details.name.replace(/[^\w\s]/gi, "");
     description = details.description;
@@ -723,7 +735,7 @@ export async function fetchNextXls20(nextXls20: any[]): Promise<any> {
   try {
     const nextNfts = await Promise.all(
       nextXls20.map(async (nft: any) => {
-        const one = await getOneXls(nft);
+        const one = await getOneXls20(nft);
         return one;
       })
     );
@@ -739,7 +751,7 @@ export async function fetchNextXls20WithSellOffer(
   const nextNfts = await Promise.all(
     nextXls20.map(async (nft: any) => {
       const { NFTokenID } = nft;
-      const schema = await getOneXls(nft);
+      const schema = await getOneXls20(nft);
       const sellOffersResponse = await fetchSellOffers(NFTokenID);
       const buyOffersResponse = await fetchBuyOffers(NFTokenID);
       const now = Date.now();
@@ -930,17 +942,17 @@ interface list {
   [name: string]: any | undefined;
 }
 const ipfsGatewayLisWithObfuscateTime: any[] = [
-  { domain: "https://dweb.link/", obfuscateTime: null },
-  { domain: "https://nftstorage.link/", obfuscateTime: null },
+  { domain: "https://dweb.link/ipfs/", obfuscateTime: null },
+  { domain: "https://nftstorage.link/ipfs/", obfuscateTime: null },
   {
-    domain: "https://ipfs.io/",
+    domain: "https://ipfs.io/ipfs/",
     obfuscateTime: null,
   },
   {
-    domain: "https://ipfs.eth.aragon.network/",
+    domain: "https://ipfs.eth.aragon.network/ipfs/",
     obfuscateTime: null,
   },
-  { domain: "https://gateway.ipfs.io/", obfuscateTime: null },
+  { domain: "https://gateway.ipfs.io/ipfs/", obfuscateTime: null },
 ];
 
 function initIpfsGatewayLisWithObfuscateTime() {
@@ -988,7 +1000,7 @@ async function recursiveIpfsFetch(url: string): Promise<any> {
   const availableIpfsGateway = getAvailableIpfsGateway();
   if (availableIpfsGateway.length) {
     const pomises = availableIpfsGateway.map((u: any) =>
-      fetch(u.domain + "ipfs/" + url, {
+      fetch(u.domain + url, {
         signal,
         cache: "force-cache",
         mode: "cors",
@@ -1114,11 +1126,11 @@ async function getIpfsJson(url: string) {
 // }
 export async function getIpfsMedia(url: string) {
   const ipfsGatewayList = [
-    "https://cloudflare-ipfs.com/",
-    "https://cf-ipfs.com/",
-    "https://nftstorage.link/",
-    "https://jorropo.net/",
-  ].map((u) => u + "ipfs/" + url);
+    "https://cloudflare-ipfs.com/ipfs/",
+    "https://cf-ipfs.com/ipfs/",
+    "https://nftstorage.link/ipfs/",
+    "https://jorropo.net/ipfs/",
+  ].map((u) => u + url);
   const controller = new AbortController();
   const signal = controller.signal;
   const pomises = ipfsGatewayList.map((u: string) =>
