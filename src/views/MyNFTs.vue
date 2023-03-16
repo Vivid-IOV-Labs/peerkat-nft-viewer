21b3894b10e8241a7aba8e1667c3fcd22c0eca87
bolladaniele <bolladaniele@gmail.com>
2023-03-16 13:43:37 +0100

    test

--- src/views/MyNFTs.vue
+++ src/views/MyNFTs.vue
@@ -44,12 +44,12 @@
       </li>
     </ul>
   </div>
-  <external-link
+  <!-- <external-link
     class="mt-1 rounded flex-center text-center d-flex"
     url="https://docs.google.com/forms/d/e/1FAIpQLSdx3iena1rUPaHrj99kDuI0wEaoR5FV3nkWVVsaeouppUZ4_w/viewform"
   >
     <img class="w-100" src="/banner-primary.png" />
-  </external-link>
+  </external-link> -->
 </template>
 
 <script lang="ts">
