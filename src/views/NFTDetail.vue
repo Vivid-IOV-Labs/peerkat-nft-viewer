df3ce76594fa245c938fae835b47e510b710b544
bolladaniele <bolladaniele@gmail.com>
2023-05-10 11:18:53 +0100

    disable view on detail

--- src/views/NFTDetail.vue
+++ src/views/NFTDetail.vue
@@ -8,14 +8,16 @@
       <base-card v-if="nft">
         <template #picture>
           <figure style="overflow: hidden">
-            <a
+            <load-media :nft="nft"></load-media>
+
+            <!-- <a
               class="h-100 d-block"
               style="overflow: hidden"
               href="#"
               @click.prevent="view"
             >
               <load-media :nft="nft"></load-media>
-            </a>
+            </a> -->
           </figure>
         </template>
 
