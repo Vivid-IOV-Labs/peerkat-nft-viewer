864e14e4b6ceecc55da2ee10a56544789e9fb1db
bolladaniele <bolladaniele@gmail.com>
2023-09-26 13:02:40 +0100

    wip

--- src/store/modules/xumm/actions.ts
+++ src/store/modules/xumm/actions.ts
@@ -2,12 +2,20 @@ import { ActionTree } from "vuex";
 import { xAppOttData } from "xumm-sdk/dist/src/types";
 import { XummState } from "./state";
 import XummService from "../../../services/XummService";
+import { getNodeTypeFromNetwork } from "../../../utils/getNetworkTypeFromCode";
 
 const actions: ActionTree<XummState, xAppOttData> = {
   async getOttData({ commit }): Promise<void> {
     try {
-      const OttData = await XummService.getOttData();
-      commit("setOttData", OttData);
+      const ottdata = await XummService.getOttData();
+      if (ottdata && ottdata.nodewss) {
+        commit("setOttData", ottdata);
+        commit("user/setAddress", ottdata.account, { root: true });
+        commit("user/setNetwork", ottdata.nodewss, { root: true });
+        const nodetype = getNodeTypeFromNetwork(ottdata.nodewss);
+        commit("user/setNodeType", nodetype, { root: true });
+        commit("user/setUser", ottdata.user, { root: true });
+      }
     } catch (error) {
       console.log("OTTDATA vuex", error);
     }
